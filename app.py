d4d3c14cd0b83766839f07db37fa1cef064191bf
Alonzo Morales <49086724+Alonzo23@users.noreply.github.com>
2019-10-15 21:34:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 12 11:20:51 2019
-
-@author: chrismiller
-"""
-
 # import necessary libraries
 from sqlalchemy import func
 import pandas as pd
@@ -19,20 +11,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/crime_data.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/crime_income_pop.db"
 
 db = SQLAlchemy(app)
 
 
 class HateCrime(db.Model):
-    __tablename__ = 'hatecrime'
-    
-    INCIDENT_ID = db.Column(db.Integer)
-    DATA_YEAR = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'crime'
+
+    INCIDENT_ID= db.Column(db.Integer, primary_key=True)
+    DATA_YEAR = db.Column(db.String)
     STATE_NAME = db.Column(db.String)
     REGION_NAME = db.Column(db.String)
     OFFENDER_RACE = db.Column(db.String)
-    OFFENSE_NAME = db.Column(db.String)
     BIAS_DESC = db.Column(db.String)
 
     def __repr__(self):
@@ -56,10 +47,15 @@ def home():
 # Query the database and return the jsonified results
 @app.route("/data")
 def data():
-     sel = [HateCrime.DATA_YEAR, func.count(HateCrime.DATA_YEAR)]
-     results = db.session.query(*sel).\
-        group_by([[DATA_YEAR], [STATE_NAME]]).all()
-    df = pd.DataFrame(results, columns=['year', 'counts'])
+    print (HateCrime.DATA_YEAR)
+    print (func.strftime(HateCrime.DATA_YEAR))
+    sel = [func.strftime(HateCrime.DATA_YEAR), HateCrime.DATA_YEAR]
+    print (sel)
+    results = db.session.query(*sel)
+    #     group_by(func.strftime("%Y", HateCrime.DATA_YEAR)).all()
+    print (results)
+    df = pd.DataFrame(results, columns=['year', 'sightings'])
+    print(df)
     return jsonify(df.to_dict(orient="records"))
 
 
